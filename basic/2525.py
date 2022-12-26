24a4a8ccb81006251135a85094b83ef794a2c1a8
noeyah2 <hy0708@cau.ac.kr>
2022-12-27 00:21:05 +0900

    1226

--- basic/2525.py
+++ basic/2525.py
@@ -1,10 +1,10 @@
 h,m = map(int, input().split())
 timer = int(input())
-h += timer//60
-m += timer % 60
+h += timer//60  #h = h+timer
+m += timer%60   #m = m+timer
 if m >= 60:
-    h += 1
-    m -= 60
+    h += 1  #h = h+1
+    m -= 60 #m = m-60
 elif h >= 24:
     h -= 24
 print(h,m)
